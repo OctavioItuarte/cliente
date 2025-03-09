4f61926ec30652ec5f275109211c0a0b920204f9
Octavio Ituarte <oituarte@alumnos.exa.unicen.edu.ar>
2025-03-08 22:18:17 -0300

    agregado nuevos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarproductosComponent } from './components/listarproductos/listarproductos.component';
+import { ListarUsuariosComponent } from './components/listar-usuarios/listar-usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'crear-producto', component: CrearProductoComponent },
+    { path: 'listar-producto', component: ListarproductosComponent },
+    { path: 'listar-usuarios', component: ListarUsuariosComponent }
+    //{ path: '', component: Login },
+    //{ path: '**',  redirectTo: '', pathMatch: 'full'}
+];
---
+++ src/app/components/listarproductos/listarproductos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listarproductos',
+  imports: [],
+  templateUrl: './listarproductos.component.html',
+  styleUrl: './listarproductos.component.css'
+})
+export class ListarproductosComponent {
+
+}
